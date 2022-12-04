be8a8d491ca93323c373b1c227a1118db44d33e0
seshotake <116971836+seshotake@users.noreply.github.com>
2022-12-04 15:19:10 +0000

    lab5

--- src/lab5.R
+++ src/lab5.R
@@ -8,15 +8,28 @@ data(pressure)
 pressure_relation <- lm(pressure ~ temperature, data = pressure)
 summary(pressure_relation)
 
-x <- c(
-    1, 3, 6, 8, 9, 10, 11, 12, 14, 16, 17, 18,
-    20, 22, 23, 24, 25, 27, 29, 30
-)
-y <- c(
-    30, 0, -210, -530, -762, -1050, -1400, -1818, -2882, -4290, -5138,
-    -6090, -8330, -11058, -12620, -14322, -16170, -20328, -25142, -27810
+
+data <- data.frame(
+    x = c(
+        1, 3, 6, 8, 9, 10, 11, 12, 14, 16, 17, 18,
+        20, 22, 23, 24, 25, 27, 29, 30
+    ),
+    y = c(
+        30, 0, -210, -530, -762, -1050, -1400, -1818, -2882, -4290, -5138,
+        -6090, -8330, -11058, -12620, -14322, -16170, -20328, -25142, -27810
+    )
 )
 
-plot(x, y)
+print(round(cor(data), 2))
 
-print(cor(x, y))
+plot(data$x, data$y)
+
+pairs(data[, c("x", "y")])
+
+print(cor(data$x, data$y))
+
+relation <- lm(y ~ x, data = data)
+print(summary(relation))
+
+print(cor(data$x, data$y, method = "pearson"))
+print(cor(data$x, data$y, method = "spearman"))
